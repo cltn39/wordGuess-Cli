88349fc9f8f6841358717d2476d30b3eceec23b5
gravitation <chisong@ucdavis.edu>
2019-05-23 13:36:47 -0700

    Letter.js codes implented based on pseudo code

--- Letter.js
+++ Letter.js
@@ -1,12 +1,24 @@
 class Letter {
-    constructor(underlying_character, IsLetterGuessed) {
-        this.underlying_character = underlying_character
-        this.IsLetterGuessed = IsLetterGuessed
+    constructor(underlying_character) {
+        this.underlying_character = underlying_character.split("");
+        this.IsLetterGuessed = false;
     }
-    letterGuess () {
+    letterGuess() {
         //A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+        if (!this.IsLetterGuessed) {
+            return '_';
+        } else if (this.IsLetterGuessed) {
+            return this.underlying_character;
+        } else {
+            console.error("error occured!")
+        }
     }
-    updateGuess () {
+    updateGuess(choice) {
         //A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+        if (choice === this.underlying_character) {
+            this.IsLetterGuessed =true;
+        }
     }
 }
+
+module.exports = Letter;
